aca4002993da0ef4e1b743d452234754aee9c1d8
m-tartag <mattbrs51@gmail.com>
2019-07-22 13:16:39 -0400

    http req with graphql/axios

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,8 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
+// const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-
-const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '88', firstName: 'Trent', age: 50 },
-];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -24,7 +20,12 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return (
+          axios
+            .get(`http://localhost:3000/users/${args.id}`)
+            // Very important below
+            .then(resp => resp.data)
+        );
       },
     },
   },
