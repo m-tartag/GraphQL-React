88e6cd8da2c8e860512a0f89f4d6ef3f8ad4cf45
m-tartag <mattbrs51@gmail.com>
2019-07-23 10:20:38 -0400

    update

--- schema/schema.js
+++ schema/schema.js
@@ -2,28 +2,49 @@ const graphql = require('graphql');
 const axios = require('axios');
 // const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+} = graphql;
 
 // keep company first because it serves info to User
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
-  },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost3000/companies/${parentValue.id}/users`)
+          .then(res => res.data);
+      },
+    },
+  }),
 });
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
     // treat associations between different schemas.. as if they were another field
-    company: { type: CompanyType },
-  },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res => res.data);
+      },
+    },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -37,8 +58,17 @@ const RootQuery = new GraphQLObjectType({
           axios
             .get(`http://localhost:3000/users/${args.id}`)
             // Very important below
-            .then(resp => resp.data)
+            .then(res => res.data)
         );
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then(res => res.data);
       },
     },
   },
